ce9865ab8ab6afd95ff038efae970e4f938abee9
sonic <sonic@MARK-3>
2021-02-08 00:34:02 +0200

    chart

--- adminHeader.php
+++ adminHeader.php
@@ -6,8 +6,8 @@
 			<a href="adminHeatmap.php"><i class="fa fa-map"></i> Heatmap</a> 
             <a href="1chart.php"><i class="fa fa-bar-chart" aria-hidden="true"></i> Ερωτημα 1</a>
             <a href="2chart.php"><i class="fa fa-bar-chart" aria-hidden="true"></i> Ερωτημα 2</a>
-            <a href="1chart.php"><i class="fa fa-bar-chart" aria-hidden="true"></i> Ερωτημα 3</a>
-            <a href="1chart.php"><i class="fa fa-bar-chart" aria-hidden="true"></i> Ερωτημα 4</a>
+            <a href="3chart.php"><i class="fa fa-bar-chart" aria-hidden="true"></i> Ερωτημα 3</a>
+            <a href="4chart.php"><i class="fa fa-bar-chart" aria-hidden="true"></i> Ερωτημα 4</a>
 			<a href="amdinUserInfo.php"><i class="fa fa-fw fa-user"></i>User Info</a>
 		</nav>
 	</div>
